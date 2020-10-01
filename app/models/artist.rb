8da2008c2ed88f615157a048a30d429a83b96586
popkorn96 <mpaloma2014@gmail.com>
2020-10-01 10:44:02 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,5 @@ class Artist < ActiveRecord::Base
     has_many :songs
     def song_count
         self.songs.count
-      end
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   def artist_name
     self.artist.name
-end
-def artist_and_title
+  end
+  def artist_and_title
     artist_name + " - " + self.title
-end
-
+  end
 end
